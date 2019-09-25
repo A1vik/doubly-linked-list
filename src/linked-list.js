4ac147938632948f598e4003a665133f7d7f3720
A1vik <A11vik@ya.ru>
2019-09-25 16:00:21 +0400

    feat: complete some tasks

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,21 +1,56 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = 0;
+        this._tail = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        this.length++;
 
-    head() {}
+        const node = new Node(data);
 
-    tail() {}
+        if (!this._head) {
+            this._head = node;
+            this._tail = node;
+        }
+        this._tail.next = data;
+        node.prev = this._tail.data;
+        this._tail = node;
+        console.log(node);
+        return this;
+    }
+
+    head() {
+        if (!this._head) {
+            return null;
+        }
+        return this._head.data;
+    }
+
+    tail() {
+        if (!this._tail) {
+            return null;
+        }
+        return this._tail.data;
+    }
 
     at(index) {}
 
     insertAt(index, data) {}
 
-    isEmpty() {}
+    isEmpty() {
+        return this.length === 0;
+    }
 
-    clear() {}
+    clear() {
+        this._head = 0;
+        this._tail = 0;
+        this.length = 0;
+        return this;
+    }
 
     deleteAt(index) {}
 
